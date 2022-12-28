0b316d6a06ce13bd0918f26eb72d6c1ed8646300
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2022-12-28 20:44:06 +0530

    fixes

--- index.ts
+++ index.ts
@@ -8,12 +8,39 @@ import { tmpdir } from 'os';
 import { pipeline } from 'stream/promises';
 import got from 'got';
 import { createWriteStream } from 'fs';
+import commandExists from 'command-exists';
 
 async function downloadTar(url: string) {
   const tempFile = join(tmpdir(), `create-fuel-dapp.temp-${Date.now()}`);
   await pipeline(got.stream(url), createWriteStream(tempFile));
   return tempFile;
 }
+
+const printWarningIfDockerNotInstalled = async () => {
+  commandExists('docker').catch(() => {
+    console.log(
+      chalk.yellow(
+        '⚠️ Docker is not installed. You need Docker to run a local Fuel node.'
+      )
+    );
+    console.log(
+      'To install Docker, follow the instructions here: https://docs.docker.com/get-docker/'
+    );
+  });
+};
+
+const printWarningIfForcNotInstalled = async () => {
+  commandExists('forc').catch(() => {
+    console.log(
+      chalk.yellow(
+        '⚠️ Forc is not installed. You need forc to build and deploy your contracts.'
+      )
+    );
+    console.log(
+      `To install forc, run: curl --proto '=https' --tlsv1.2 -sSf https://fuellabs.github.io/fuelup/fuelup-init.sh | sh`
+    );
+  });
+};
 
 const main = async () => {
   let projectPath = '';
@@ -47,7 +74,7 @@ const main = async () => {
   console.log('To get started:');
   console.log();
   console.log(`- cd into the project directory: cd ${projectPath}`);
-  console.log('- Start a local Fuel node by running `make services-start`');
+  console.log('- Start a local Fuel node by running `make services-run`');
   console.log(
     '- Build and deploy the contract by running `cd contracts && forc deploy --url 0.0.0.0:4000 --unsigned`'
   );
@@ -59,6 +86,13 @@ const main = async () => {
   console.log(
     '-> If you have any questions, ask us on our Discord: https://discord.com/invite/xfpK4Pe'
   );
+  console.log();
+  console.log();
+
+  await printWarningIfDockerNotInstalled();
+  console.log();
+  console.log();
+  await printWarningIfForcNotInstalled();
 };
 
 main().catch((err) => {
